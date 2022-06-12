84c01bf5567cc47b5e2bfcbee9789d26f724a7aa
Steven <shnauzerr@hotmail.com>
2022-06-12 13:51:19 -0600

    Try except added to function 2. v0.1.1

--- ec2_functions.py
+++ ec2_functions.py
@@ -56,57 +56,66 @@ def instance_report():
 
 def ingress_rules(user_group_raw):
 
-    # Strip whitespaces off entry
-    user_group = user_group_raw.strip()
+    try:
+        # Strip whitespaces off entry
+        user_group = user_group_raw.strip()
 
-    # Pull security group description
-    aws = boto3.client('ec2') 
-    describe_output = aws.describe_security_groups()
-    security_group_list = describe_output["SecurityGroups"]
+        try:
+            # Pull security group description
+            aws = boto3.client('ec2') 
+            describe_output = aws.describe_security_groups()
+            security_group_list = describe_output["SecurityGroups"]
+        except Exception as e:
+            print(f"\nError retrieving aws data. Please check your connection and/or configure the settings on the cli via 'aws configure'. The error: {e}\n")
+            exit(1)
 
-    # Iterate through all security groups to find the specified one
-    desired_security_group = {}
-    for security_group in security_group_list:
-        if security_group["GroupId"] == user_group:
-            desired_security_group = security_group
-    
-    # Check if security group has been found
-    if desired_security_group == {}:
-        print(f'\nUnable to find security group {user_group}.')
-        return []
+        # Iterate through all security groups to find the specified one
+        desired_security_group = {}
+        for security_group in security_group_list:
+            if security_group["GroupId"] == user_group:
+                desired_security_group = security_group
+        
+        # Check if security group has been found
+        if desired_security_group == {}:
+            print(f'\nUnable to find security group {user_group}.')
+            return []
 
-    # Display the ingress rules for that security group showing the CIDR and port
-    ip_permissions_list = desired_security_group["IpPermissions"]
-    rules_total = []
-    warn = False
-    for ip_permission in ip_permissions_list:
-        port = ip_permission["FromPort"]
-        cidr_group = ip_permission["IpRanges"]
-        for cidr_iter in cidr_group:
-            cidr = cidr_iter["CidrIp"]
-            rules_total.append({"Security Group ID": user_group, "port": port, "Cidr": cidr})
+        # Display the ingress rules for that security group showing the CIDR and port
+        ip_permissions_list = desired_security_group["IpPermissions"]
+        rules_total = []
+        warn = False
+        for ip_permission in ip_permissions_list:
+            port = ip_permission["FromPort"]
+            cidr_group = ip_permission["IpRanges"]
+            for cidr_iter in cidr_group:
+                cidr = cidr_iter["CidrIp"]
+                rules_total.append({"Security Group ID": user_group, "port": port, "Cidr": cidr})
 
-            # Display a warning if any of the rules in the security group allow for inbound traffic from any IP address
-            if "127.0.0.0" in cidr:
-                warn = True
-            elif "0.0.0.0" in cidr:
-                warn = True
+                # Display a warning if any of the rules in the security group allow for inbound traffic from any IP address
+                if "127.0.0.0" in cidr:
+                    warn = True
+                elif "0.0.0.0" in cidr:
+                    warn = True
 
-    # Print Results
-    print('\nIngress Rules:')
-    str_format = "{:<24} {:<20} {:<10}"
-    print("----------------------------------------------------------------------------------")
-    print(str_format.format('Security Group ID', 'CIDR', 'Port'))
-    for rule in rules_total:
-        print(str_format.format(rule['Security Group ID'], rule['Cidr'], rule['port']))
-    print("----------------------------------------------------------------------------------")
+        # Print Results
+        print('\nIngress Rules:')
+        str_format = "{:<24} {:<20} {:<10}"
+        print("----------------------------------------------------------------------------------")
+        print(str_format.format('Security Group ID', 'CIDR', 'Port'))
+        for rule in rules_total:
+            print(str_format.format(rule['Security Group ID'], rule['Cidr'], rule['port']))
+        print("----------------------------------------------------------------------------------")
 
-    # Print warning for traffic from any IP
-    if warn is True:
-        print("WARNING: Rule(s) in the security group allows for inbound traffic from ANY IP address.")
+        # Print warning for traffic from any IP
+        if warn is True:
+            print("WARNING: Rule(s) in the security group allows for inbound traffic from ANY IP address.")
 
-    # Return for testing
-    return [rules_total, warn]
+        # Return for testing
+        return [rules_total, warn]
+
+    except Exception as e:
+        print(f"\nUnknown error: {e}\n")
+        exit(1)
 
 
 
